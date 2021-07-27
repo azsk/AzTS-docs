82c476e23c28da606dbbaccc2ddc667938310484
RohitYadav-msft <53034720+RohitYadav-msft@users.noreply.github.com>
2021-07-27 14:17:58 +0530

    Update Helper.ps1
    align text

--- Scripts/RemediationScripts/Helper.ps1
+++ Scripts/RemediationScripts/Helper.ps1
@@ -55,9 +55,9 @@ class ResourceResolver
 				#print the message saying these RGS provided in excludeRGS are not found
 				Write-Host "Warning: Following resource group(s) requested for exclusion not found in subscription:" -ForegroundColor Yellow
 				Write-Host "ResourceGroupName"
-                Write-Host "-----------------"
-                Write-Host "$($nonExistingRGS -join "`n")"
-                Write-Host "`n"
+				Write-Host "-----------------"
+				Write-Host "$($nonExistingRGS -join "`n")"
+				Write-Host "`n"
 			}
 
 			if(($matchingRGs| Measure-Object).Count -gt 0 )
@@ -70,13 +70,13 @@ class ResourceResolver
 					{
 						# Updating ExcludeResourceNames with non-coinciding resources
 						$this.ExcludeResourceNames = $this.ExcludeResourceNames | Where-Object {$_ -notin $coincidingResources.ResourceName}
-						
+
 						# Adding coinciding resources in ExcludedResources
 						# ExcludedResources contain list of resources which are requested to exclude
 						$this.ExcludedResources += $coincidingResources
 						$this.messageToPrint += "Number of resources excluded due to excluding resource groups: $(($coincidingResources | Measure-Object).Count)"
 						$this.messageToPrint += "$($coincidingResources | Select-Object -Property "ResourceGroupName", "ResourceName"| Sort-Object |Format-Table |Out-String)"
-						
+
 						# Updating matchingRGs with RGs of non-coinciding resources
 						$matchingRGs = $matchingRGs | Where-Object { $_ -notin $coincidingResources.ResourceGroupName }
 					}
@@ -84,12 +84,12 @@ class ResourceResolver
 
 				# If no coinciding resource found then need to exclude given resource group name
 				$this.ExcludedResources += $Resources| Where-Object{$_.ResourceGroupName -in $matchingRGs}
-                $this.messageToPrint += "Number of resource group(s) excluded explicitly: $(($matchingRGs | Measure-Object).Count)"
-                $this.messageToPrint += "ResourceGroupName"
-                $this.messageToPrint += "-----------------"				
-                $this.messageToPrint += "$($matchingRGs | Sort-Object |Format-Table |Out-String)"
-                $this.messageToPrint += "`n"
-				}
+				$this.messageToPrint += "Number of resource group(s) excluded explicitly: $(($matchingRGs | Measure-Object).Count)"
+				$this.messageToPrint += "ResourceGroupName"
+				$this.messageToPrint += "-----------------"				
+				$this.messageToPrint += "$($matchingRGs | Sort-Object |Format-Table |Out-String)"
+				$this.messageToPrint += "`n"
+			}
 		}
 		
 		$ExcludedRes = @();
@@ -106,9 +106,9 @@ class ResourceResolver
 				$ResourcesToExclude = $this.ExcludeResourceNames | Where-Object{ $_ -notin $NonExistingResource }
 				Write-Host "Warning: Following resource(s) requested for exclusion not found in subscription:" -ForegroundColor Yellow
 				Write-Host "ResourceName"
-                Write-Host "------------"
-                Write-Host "$($NonExistingResource -join "`n")"
-                Write-Host "`n"
+				Write-Host "------------"
+				Write-Host "$($NonExistingResource -join "`n")"
+				Write-Host "`n"
 			}	
 			
 			$ExcludedRes = $Resources | Where-Object{$_.ResourceName -in $ResourcesToExclude}
