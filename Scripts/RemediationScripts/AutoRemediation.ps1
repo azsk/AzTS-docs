23576de62b7c286762330ea85749ca14d8dd2045
Saurabh Gautam <sagautam@microsoft.com>
2022-08-12 11:58:10 +0530

    Updated AutoRemediation script to handle unwanted scenarios

--- Scripts/RemediationScripts/AutoRemediation.ps1
+++ Scripts/RemediationScripts/AutoRemediation.ps1
@@ -154,6 +154,8 @@ Enter the choice (1|2)";
                 }elseif ($control.ControlId -eq "Azure_AppService_DP_Use_Secure_TLS_Version") {
                     $commandString = $control.InitCommand + " -SubscriptionId " +  "`'" + $SubscriptionId +  "`'" +  " -Path " + "`'" + "FailedControls\" +  $SubscriptionId + ".json" + "`'" + " -PerformPreReqCheck"+ " -AutoRemediation" + " -TimeStamp " + "`'" + $timeStampString +  "`'";
                 }else{
+                    Write-Host "Skipped remediation of failing resources of control id: [$($control.ControlId)], because remediation support for this control hasn't been added yet." -ForegroundColor $([Constants]::MessageType.Warning)
+                    Write-Host $([Constants]::SingleDashLine)
                     continue;
                 }
 
