69896e96ec94454d333153dbadc1819c4b8f3de7
Richa Jaiswal <v-rijaiswal@microsoft.com>
2023-02-17 16:34:51 +0530

    Removed the id

--- Scripts/RemediationScripts/AutoRemediation.ps1
+++ Scripts/RemediationScripts/AutoRemediation.ps1
@@ -163,7 +163,6 @@ Enter the choice (1|2)";
                 ($control.ControlId -eq "Azure_ServiceBus_DP_Use_Secure_TLS_Version") -or
                 ($control.ControlId -eq "Azure_DBForMySQLFlexibleServer_DP_Use_Secure_TLS_Version") -or
                 ($control.ControlId -eq "Azure_SQLDatabase_DP_Enable_TDE") -or
-                ($control.ControlId -eq "Azure_FrontDoor_DP_Use_Secure_TLS_Version_Trial")-or
                 ($control.ControlId -eq "Azure_EventHub_DP_Use_Secure_TLS_Version")){
                     $commandString = $control.InitCommand + " -SubscriptionId " +  "`'" + $SubscriptionId +  "`'" +  " -Path " + "`'" + "FailedControls\" +  $SubscriptionId + ".json" + "`'" + " -PerformPreReqCheck"+ " -AutoRemediation" + " -TimeStamp " + "`'" + $timeStampString +  "`'";
                 }elseif ($control.ControlId -eq "Azure_KubernetesService_AuthN_Enabled_AAD") {
