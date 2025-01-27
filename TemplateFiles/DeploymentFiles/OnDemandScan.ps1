9cacb4c84a130d12aee42d3c1f20028f6ad62b2c
kanika-1007 <kpmittal505@gmail.com>
2025-01-27 14:23:39 +0530

    Update OnDemandScan.ps1

--- TemplateFiles/DeploymentFiles/OnDemandScan.ps1
+++ TemplateFiles/DeploymentFiles/OnDemandScan.ps1
@@ -1,4 +1,4 @@
-﻿########### Load Common Functions And Classes ###############
+########### Load Common Functions And Classes ###############
 
 function Start-AzSKTenantSecuritySolutionOnDemandScan
 {
